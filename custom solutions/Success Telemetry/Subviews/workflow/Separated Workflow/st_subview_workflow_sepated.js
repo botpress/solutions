b7807fe67b65f93c85d40d1586268de3c2d7afcf
David Vitor Antonio <david.vitora@gmail.com>
2022-01-21 15:57:49 -0300

    update

--- custom solutions/Success Telemetry/Subviews/workflow/Separated Workflow/st_subview_workflow_sepated.js
+++ custom solutions/Success Telemetry/Subviews/workflow/Separated Workflow/st_subview_workflow_sepated.js
@@ -1,34 +1,46 @@
-  bp.experimental.successTelemetry.addNewSubview({
-    id: 'workflow_details',
-    label: 'Workflow Details',
-    metricId: 'workflow',
-    viewFunction: async ({ botId, startDate, endDate, getFilteredQuery }) => {
-      const all = await getFilteredQuery()
+bp.experimental.successTelemetry.addNewSubview({
+  id: "workflow_details",
+  label: "Workflow Details",
+  metricId: "workflow",
+  viewFunction: async ({ botId, startDate, endDate, getFilteredQuery }) => {
+    const all = await getFilteredQuery();
 
-      return `
-        <table>
-          <tr>
-            <th> Workflow ID </th>
-            <th> Status </th>
-            <th> Start Unix </th>
-            <th> End Unix </th>
-          </tr>
-          <tr>
-            ${all
-              .map(item => {
-                return `
-                <tr>
-                  <td>${item.payload.workflowId}</td>
-                  <td>${item.payload.status}</td>
-                  <td>${item.payload.startUnix}</td>
-                  <td>${item.payload.endUnix}</td>
-                </tr>
-              `
-              })
-              .join('')}
-          </tr>
-        </table>
-      `
+    const workflows = {};
+
+    for (const item of all) {
+      const { workflowId, status } = item.payload;
+      if (!workflows[workflowId]) {
+        workflows[workflowId] = { count: 0, successCount: 0 };
+      }
+
+      workflows[workflowId].count++;
+
+      if (status == "SUCCESS") workflows[workflowId].successCount++;
     }
-  })
-  //sss
+
+    return `
+      <table>
+        <tr>
+          <th> Workflow ID </th>
+          <th> Workflows Started </th>
+          <th> Successful Completion </th>
+        </tr>
+        <tr>
+          ${Object.keys(workflows)
+            .map((workflowId) => {
+              const { successCount, count } = workflows[workflowId];
+
+              return `
+                <tr>
+                  <td>${workflowId}</td>
+                  <td>${count}</td>
+                  <td>${count > 0 ? (successCount / count).toFixed(2) * 100 : "-"}%</td>
+                </tr>
+              `;
+            })
+            .join("")}
+        </tr>
+      </table>
+    `;
+  },
+});
