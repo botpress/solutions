ffbc4bb8f34b1c14e02f8eca37cbb787d07e23a7
Gordon Clark <gordon.clark@botpress.com>
2022-04-04 15:00:20 -0400

    Update Timely-Greeting.js
    Added better description and option to pass contentIds as params

--- custom solutions/Timely Greeting/Timely-Greeting.js
+++ custom solutions/Timely Greeting/Timely-Greeting.js
@@ -1,5 +1,12 @@
-/**
-   * Greets the user with either a monrning, afternoon, or evening greeting as appropriate. It can take greetings as parameters, or refrence content elements by ID, or use default greetings.
+  /**
+   * Greets the user with either a monrning, afternoon, or evening greeting as appropriate
+   * Params can be:
+   *
+   * 1. Plain text /"Good Morning"/
+   *
+   * 2. Text content element Ids /"builting_text-wofnSd"/
+   *
+   * 3. Blank (Default value of /"Good ______"/ will apply)
    * @title Timely Greeting
    * @category Custom
    * @author Botpress
@@ -9,27 +16,54 @@
    */
   const moment = require('moment')
   const _ = require('lodash')
+  const isElement = new RegExp('^builtin_text-[a-zA-Z0-9]{6}$')
+  // Define default greetings here
+  const defaultGreetings = ['Good morning', 'Good afternoon', 'Good evening']
+  // This is the default if something goes wrong
+  var message = 'Hello'
+  //Customize when each period starts or ends here.
+  const schedule = {
+    morningStart: 5,
+    morningEnd: 11,
+    afternoonEnd: 16
+  }
   const myAction = async (morning, afternoon, evening) => {
-    // Default greetings. To use existing content elements, uncomment the bp.cms and add the element ID
-    if (!morning) morning = 'Good morning ' //await bp.cms.getContentElement(event.botId, 'Your Content ID')
-    if (!afternoon) afternoon ='Good afternoon' // await bp.cms.getContentElement(event.botId, 'builtin_text-oDDwIp')
-    if (!evening) evening = 'Good evening' //await bp.cms.getContentElement(event.botId, "Your Content ID")
-    var message = 'Hello'
+    // Go through each parameter and, if it is a contentID, fetch the content element
+    var messages = [morning, afternoon, evening]
+    for (var i = 0; i < 3; i++) {
+      var m = messages[i]
+      if (!m) {
+        messages[i] = defaultGreetings[i]
+        bp.logger.info('Param is blank')
+      }
+      if (isElement.test(m)) {
+        bp.logger.info('Param is a contentId')
+        try {
+          messages[i] = await bp.cms.getContentElement(event.botId, messages[i])
+          bp.logger.info(messages[i])
+        } catch {
+          bp.logger.forBot(event.botId).error(`Timely Greeting Error: contentID ${m} does not exist`)
+        }
+      } else continue
+    }
+
     //gets the user's hour as an int from 0 to 23
     const currentTime = parseInt(
       moment()
         .zone(user.timezone.toString())
         .format('HH')
     )
-    //Modify these numbers to control when exactly morning, afternoon, and evening are
-    if (currentTime >= 0 && currentTime <= 11) {
-      message = morning
+    bp.logger.info(`current time is ${currentTime}`)
+    bp.logger.info(`Morning ends at ${schedule.morningEnd} and afternoon ends at ${schedule.afternoonEnd}`)
+
+    if (currentTime >= schedule.morningStart && currentTime <= schedule.morningEnd) {
+      message = messages[0]
     }
-    if (currentTime >= 12 && currentTime <= 16) {
-      message = afternoon
+    if (currentTime > schedule.morningEnd && currentTime <= schedule.afternoonEnd) {
+      message = messages[1]
     }
-    if (currentTime >= 17 && currentTime <= 23) {
-      message = evening
+    if (currentTime > schedule.afternoonEnd || currentTime <= schedule.morningStart) {
+      message = messages[2]
     }
     // Create the text content element
     if (message.formData) {
