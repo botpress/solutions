0f81ab462c205fad1c715b4d6c33204d630d23fa
David Vitor Antonio <david.vitora@gmail.com>
2022-04-11 16:27:19 -0300

    Update createDropoutTable.js

--- custom solutions/Record final node/createDropoutTable.js
+++ custom solutions/Record final node/createDropoutTable.js
@@ -1,8 +1,12 @@
-  function createTable() {
-    bp.database.createTableIfNotExists('last_nodes', function(table) {
-      table.string('session_id')
-      table.string('last_flow')
-      table.string('last_node')
-    })
-  }
-  createTable()
+function createTable() {
+  bp.database.createTableIfNotExists('last_nodes', function(table) {
+    table
+      .increments('id')
+      .unsigned()
+      .primary()
+    table.string('session_id')
+    table.string('last_flow')
+    table.string('last_node')
+  })
+}
+createTable()
