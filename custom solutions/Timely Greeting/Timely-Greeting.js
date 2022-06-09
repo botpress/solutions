a6f2c94ef2f05a6561226f364f82638d5b24ac00
Gordon Clark <gordon.clark@botpress.com>
2022-06-09 15:17:10 -0400

    Update Timely-Greeting.js
    1. Removed debugging logs
    2. Fixed variable & markdown rendering

--- custom solutions/Timely Greeting/Timely-Greeting.js
+++ custom solutions/Timely Greeting/Timely-Greeting.js
@@ -16,7 +16,7 @@
    */
   const moment = require('moment')
   const _ = require('lodash')
-  const isElement = new RegExp('^builtin_text-[a-zA-Z0-9]{6}$')
+  const isElement = /^builtin_text-[\S]{6}$/m
   // Define default greetings here
   const defaultGreetings = ['Good morning', 'Good afternoon', 'Good evening']
   // This is the default if something goes wrong
@@ -39,39 +39,41 @@
       if (isElement.test(m)) {
         bp.logger.info('Param is a contentId')
         try {
-          messages[i] = await bp.cms.getContentElement(event.botId, messages[i])
-          bp.logger.info(messages[i])
+          messages[i] = await bp.cms.getContentElement(event.botId, messages[i], user.language)
         } catch {
           bp.logger.forBot(event.botId).error(`Timely Greeting Error: contentID ${m} does not exist`)
         }
       } else continue
     }
-
     //gets the user's hour as an int from 0 to 23
-    const currentTime = parseInt(
-      moment()
-        .format('HH')
-    ) + parseInt(user.timezone)
-    bp.logger.info(`current time is ${currentTime}`)
-    bp.logger.info(`Morning ends at ${schedule.morningEnd} and afternoon ends at ${schedule.afternoonEnd}`)
-
+    const currentTime = parseInt(moment().format('HH')) + parseInt(user.timezone)
     if (currentTime >= schedule.morningStart && currentTime <= schedule.morningEnd) {
       message = messages[0]
-    }
-    else if (currentTime > schedule.morningEnd && currentTime <= schedule.afternoonEnd) {
+    } else if (currentTime > schedule.morningEnd && currentTime <= schedule.afternoonEnd) {
       message = messages[1]
-    }
-    else if (currentTime > schedule.afternoonEnd || currentTime <= schedule.morningStart) {
+    } else if (currentTime > schedule.afternoonEnd || currentTime <= schedule.morningStart) {
       message = messages[2]
     }
     // Create the text content element
+    // Variations aren't randomly selected when content elements are sent via action
+    // so we have to choose one manually
     if (message.formData) {
-      const allTexts = _.concat(message.formData.variations$en, message.formData.text$en)
-      var selectedText = _.sample(allTexts)
-      var payload = await bp.cms.renderElement('builtin_text', { text: selectedText, event }, event)
-    } else {
-      var payload = await bp.cms.renderElement('builtin_text', { text: message, event }, event)
+      const allTexts = _.concat(message.formData.text, message.formData.variations)
+      message = _.sample(allTexts)
     }
+    // We also need to isolate & evaluate any variables in {{double braces}}
+    let textArr = []
+    message.split(' ').forEach(word => {
+      if (/^\{\{\S+\}\}$/.test(word)) {
+        word = eval(word.substring(2, word.length - 2))
+      }
+      textArr.push(word)
+    })
+    var payload = await bp.cms.renderElement(
+      'builtin_text',
+      { type: 'text', text: textArr.join(' '), markdown: true, typing: true },
+      event
+    )
     // Send it!
     bp.events.replyToEvent(event, payload)
   }
