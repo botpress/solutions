4b4f2eb1f53785290e628363e6649f07fd502a4e
Gordon Clark <gordon.clark@botpress.com>
2022-06-10 10:18:43 -0400

    Update increment-variable-by-n.js
    Added default value

--- custom solutions/Increment Variable By N/increment-variable-by-n.js
+++ custom solutions/Increment Variable By N/increment-variable-by-n.js
@@ -7,19 +7,19 @@
    * @param {string} name - the variable name
    * @param {int} n - the amount to increment by
    */
+  const DEFAULT_VALUE = 0
   const myAction = async (scope, name, n) => {
     if (scope === 'bot') {
       const original = await bp.kvs.forBot(event.botId).get('global')
       if (original == undefined) {
-        await bp.kvs.forBot(event.botId).set('global', { ...original, [name]: 0 })
+        await bp.kvs.forBot(event.botId).set('global', { ...original, [name]: DEFAULT_VALUE })
       } else {
-        const value = original[name] ? parseInt(original[name]) + n : 0
+        const value = original[name] ? parseInt(original[name]) + n : DEFAULT_VALUE
         await bp.kvs.forBot(event.botId).set('global', { ...original, [name]: value })
       }
       bp.logger.info(await bp.kvs.forBot(event.botId).get('global'))
     } else if (scope != undefined && scope != '') {
-      event.state[scope][name] = event.state[scope][name] ? parseInt(event.state[scope][name]) + n : 0
+      event.state[scope][name] = event.state[scope][name] ? parseInt(event.state[scope][name]) + n : DEFAULT_VALUE
     } else bp.logger.forBot(event.botId).warn(`Please specify a variable scope`)
   }
-
   return myAction(args.scope, args.name, parseInt(args.n))
