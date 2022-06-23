4b4e4a690b956cb92d6908d58158455972287389
JustusNBB <73602543+JustusNBB@users.noreply.github.com>
2022-06-23 12:05:47 +0200

    Add networkmode host to image baker buildImage.config.js

--- custom_tools/bp_image_builder/custom_buildImage_config/buildImage.config.js
+++ custom_tools/bp_image_builder/custom_buildImage_config/buildImage.config.js
@@ -1,6 +1,8 @@
 module.exports = async () => {
     return  {
-        nocache: true
+        nocache: true,
+        networkmode: 'host', // recommended as default, so building also works for local deployments
+        // for all other possible values, see: https://docs.docker.com/engine/api/v1.37/#operation/ImageBuild
     }
 }
  
