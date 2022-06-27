27b41bc4a77ca9b6196880981ad15326b7c78e79
Bassam Tantawi <bassam.tantawi@botpress.com>
2022-06-27 20:45:52 +0200

    Removing 2 modules
    botkit & extended choice skill

--- custom modules/extended-choice-skill/src/views/full/index.jsx
+++
@@ -1,4 +0,0 @@
-/**
- * Choice component is exported in lowercase to avoid breaking existing flows and skills
- */
-export { Choice as extendedchoice } from './choice'
--- custom modules/extended-choice-skill/src/views/lite/index.jsx
+++
@@ -1,2 +0,0 @@
-export { QuickReplies } from './QuickReplies'
-export { Button } from './Button'
