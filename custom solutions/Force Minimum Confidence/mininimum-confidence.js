9404a69fc2ee6b819845fb4c8ffbec87f80353da
Gordon Clark <gordon.clark@botpress.com>
2022-07-04 10:48:08 -0400

    Update mininimum-confidence.js

--- custom solutions/Force Minimum Confidence/mininimum-confidence.js
+++ custom solutions/Force Minimum Confidence/mininimum-confidence.js
@@ -1,7 +1,7 @@
   async function hook() {
     if (event.nlu.intent.confidence < 0.75) {
       bp.logger.info("Confidence too low, overriding...")
-      event.nlu.intent = {
+      event.nlu.intent[0] = {
         name: 'none',
         confidence: 1,
         context: 'global'
