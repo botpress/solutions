bdec30e0668d46a95a07b15709b704c50a4ce6e4
David Vitor Antonio <david.vitora@gmail.com>
2022-08-17 17:34:36 -0300

    update script

--- custom solutions/Conversation Events Webhook/conversation_events_webhook.js
+++ custom solutions/Conversation Events Webhook/conversation_events_webhook.js
@@ -1,35 +1,46 @@
-  const MAX_ATTEMPTS = 10
+const MAX_ATTEMPTS = 10
 
-  const config = {
-    url: 'http://localhost:3500/handler',
-    headers: {}
-  }
+const config = {
+  // Change this value to point to your own webhook handler
+  url: 'http://localhost:3500/handler',
+  // Change this object to specify additional headers to be sent in the request
+  headers: {}
+}
 
-  const { backOff } = require('exponential-backoff')
-  const axios = require('axios')
+const { backOff } = require('exponential-backoff')
+const axios = require('axios')
 
-  async function send(data) {
-    if (this.disabled) {
-      return
-    }
+async function logWebhookError(e, url, message) {
+  bp.logger.warn(message, {
+    url,
+    message: e.message,
+    response: e.response && e.response.data,
+    status: e.response && e.response.status
+  })
+}
 
-    const { url, headers } = config
-    const axios_config = { headers }
+async function send(data) {
 
-    try {
-      await backOff(async () => axios.post(url, data, axios_config), {
-        jitter: 'none',
-        numOfAttempts: MAX_ATTEMPTS,
-        retry: (e, attemptNumber) => {
-          if (attemptNumber === 1 && e.response && e.response.status !== 503) {
-            this.logWebhookError(e, url, 'Failed to send webhook event on first attempt. Retrying 9 more times')
-          }
-          return true
+  const { url, headers } = config
+  const axios_config = { headers }
+
+  try {
+    await backOff(async () => axios.post(url, data, axios_config), {
+      jitter: 'none',
+      numOfAttempts: MAX_ATTEMPTS,
+      retry: (e, attemptNumber) => {
+        if (attemptNumber === 1 && e.response && e.response.status !== 503) {
+          logWebhookError(e, url, 'Failed to send webhook event on first attempt. Retrying 9 more times')
         }
-      })
-    } catch (e) {
-      this.logWebhookError(e, url, `Unable to send webhook event after ${MAX_ATTEMPTS} attempts`)
-    }
+        return true
+      }
+    })
+  } catch (e) {
+    logWebhookError(e, url, `Unable to send webhook event after ${MAX_ATTEMPTS} attempts`)
   }
+}
 
-  return send(event)
+// Comment the line below if you don't want the dialog engine to wait your server to receive the event first
+return send(event)
+// Uncomment the line below if you don't want the dialog engine to wait your server to receive the event first
+// send(event)
