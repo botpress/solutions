047488e74c7ca03d30f9f2901b6716fe269009fd
David Vitor Antonio <david.vitora@gmail.com>
2022-09-06 10:46:08 -0300

    add(solution): expose public files

---
+++ custom solutions/Expose public files/hooks/after_server_start/create_files_router.js
@@ -0,0 +1,22 @@
+  const router = bp.http.createRouterForBot('files', { checkAuthentication: false })
+  const mime = require('mime-types')
+
+  router.get('/:file', async (req, res) => {
+    try {
+      if (!(await bp.ghost.forRoot().fileExists('/myfiles', req.params.file))) {
+        res.status('404')
+        res.send('File not found')
+        return
+      }
+
+      const file = await bp.ghost.forRoot().readFileAsBuffer('/myfiles', req.params.file)
+      if (req.query.download == 'true') {
+        res.setHeader('Content-Disposition', 'attachment; filename=' + req.params.file)
+      }
+      res.type(mime.lookup(req.params.file) || 'text/plain')
+      res.end(file)
+    } catch (e) {
+      res.status('500')
+      res.send('Failed to get file: ' + e.message)
+    }
+  })
---
+++ custom solutions/Expose public files/readme.md
@@ -0,0 +1,11 @@
+# Create logout link
+
+This solution will create a url that can be used to expose files from a public folder
+
+# How to install
+
+1. Create the after_server_start hook available in this folder (create_files_router.js) as a global hook in your server, and restart your server
+2. Create a folder called 'myFiles' in the root using the advanced editor (only)
+2.1. In the code editor, click on 'Advanced Editor'
+# How to use it
+
