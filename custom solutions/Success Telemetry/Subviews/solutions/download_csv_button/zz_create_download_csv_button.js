2e69655392544be34996de2e415fb37ec41a0143
David Vitor Antonio <david.vitora@gmail.com>
2022-12-19 17:02:00 -0300

    Update zz_create_download_csv_button.js

--- custom solutions/Success Telemetry/Subviews/solutions/download_csv_button/zz_create_download_csv_button.js
+++ custom solutions/Success Telemetry/Subviews/solutions/download_csv_button/zz_create_download_csv_button.js
@@ -1,83 +1,117 @@
   const moment = require('moment')
 
-  for (const metricId of Object.keys(bp.experimental.successTelemetryRegistry)) {
-    if (metricId != 'others') {
-      const metric = bp.experimental.successTelemetryRegistry[metricId]
-      bp.experimental.successTelemetry.addNewSubview({
-        id: 'export-' + metric.key,
-        label: 'Interactions',
-        metricId: metric.key,
-        viewFunction: async ({ botId, startDate, endDate }) => {
-          const startDateString = moment(startDate)
-            .local()
-            .format('D_MMM_YYYY')
-          const endDateString = moment(endDate)
-            .local()
-            .format('D_MMM_YYYY')
-          // In order for this script to work ; is required after every command, if not
-          // you will get a "Unexpected token ..." error
-          // - Remove comments too
-          // - The \ character should be excaped with \\
-          const script = `
-            function download_${metric.key}() {
-              const tableSelector = "${metric.options.name}";
-              var aTags = document.getElementsByTagName("h2");
-              var searchText = tableSelector;
-              var found;
-
-              for (var i = 0; i < aTags.length; i++) {
-                if (aTags[i].textContent == searchText) {
-                  found = aTags[i];
-                  break;
-                }
-              }
-              var table = found.parentNode.getElementsByTagName('table')[0];
-
-              if(!table) {
-                alert("Could not find table for metric with title: " + tableSelector);
-                return;
-              }
-
-              var csv_data = [];
-
-              var rows = table.getElementsByTagName('tr');
-              for (var i = 0; i < rows.length; i++) {
-          
-                  var cols = rows[i].querySelectorAll('td,th');
-          
-                  var csvrow = [];
-                  for (var j = 0; j < cols.length; j++) {
-                      csvrow.push(cols[j].innerHTML);
-                  }
-          
-                  csv_data.push(csvrow.join(","));
-              }
-              csv_data = csv_data.join('\\n');
-
-              var element = document.createElement('a');
-              element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_data));
-              element.setAttribute('download', "exported_${metric.key}__bot_${botId}__from_${startDateString}__to__${endDateString}");
-
-              element.style.display = "none";
-              document.body.appendChild(element);
-
-              element.click();
-
-              document.body.removeChild(element);
-            }
-          `
-          return `
-            <script id='graph-${metric.key}' type="text/javascript">
-              ${script}
-            </script>
-            <img src onerror="
-              var scriptGraph = document.createElement('script');
-              scriptGraph.innerText = document.getElementById('graph-${metric.key}').innerText;
-              document.getElementById('graph-${metric.key}').appendChild(scriptGraph);
-            "/>
-            <button onclick="download_${metric.key}()">Download CSV</button>
-          `
-        }
-      })
+  const injectFunction = (name, functionBody, variables = {}) => {
+    return {
+      inject: `<script id='fn-${name}' type="text/javascript">
+                    ${Object.keys(variables)
+                      .map(key => {
+                        const stringVar = variables[key].toString()
+                        const value = typeof variables[key] == 'string' ? `"${stringVar}"` : stringVar
+                        return `var ${key} = ${value};`
+                      })
+                      .join(' ')}
+                    function ${name}${functionBody
+        .toString()
+        .replace(/^\(\) =>/, '()')
+        .replace(/\n/g, ';')
+        // Remove single line comments
+        .replace(/\/\/.*?;( ){1}/g, '')}
+                  </script>
+                  <img src onerror="
+                    var scriptGraph = document.createElement('script');
+                    scriptGraph.innerText = document.getElementById('fn-${name}').innerText;
+                    document.getElementById('fn-${name}').appendChild(scriptGraph);
+                  "/>`,
+      call: name + '()'
     }
   }
+
+  const generateButton = (subviewId, metricKey, h2Title) => {
+    bp.experimental.successTelemetry.addNewSubview({
+      id: 'export-' + subviewId,
+      label: `Interactions ${h2Title}`,
+      metricId: metricKey,
+      viewFunction: async ({ botId, startDate, endDate }) => {
+        const searchText = h2Title
+        const startDateString = moment(startDate)
+          .local()
+          .format('D_MMM_YYYY')
+        const endDateString = moment(endDate)
+          .local()
+          .format('D_MMM_YYYY')
+        const filename = `exported_${subviewId}__bot_${botId}__from_${startDateString}__to__${endDateString}`
+
+        const fn = injectFunction(
+          `download_${subviewId}`,
+          () => {
+            // Get all h2 tags from the page
+            var aTags = [...document.getElementsByTagName('h2'), ...document.getElementsByTagName('h5')]
+            var found
+            // Search for the one with the text from the metric title
+            for (var i = 0; i < aTags.length; i++) {
+              if (aTags[i].textContent.includes(searchText)) {
+                found = aTags[i]
+                break
+              }
+            }
+            var table = found.parentNode.getElementsByTagName('table')[0]
+            if (!table) {
+              alert('Could not find table for metric with title: ' + searchText)
+              return
+            }
+
+            // Create the csv array
+            var csv_data = []
+            var rows = table.getElementsByTagName('tr')
+            // For each row
+            for (var i = 0; i < rows.length; i++) {
+              var cols = rows[i].querySelectorAll('td,th')
+
+              var csvrow = []
+              // For each column
+              for (var j = 0; j < cols.length; j++) {
+                csvrow.push(cols[j].innerHTML)
+              }
+
+              csv_data.push(csvrow.join(','))
+            }
+
+            //Create A tag and click intiate download
+            var element = document.createElement('a')
+            element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_data.join('\n')))
+            element.setAttribute('download', filename)
+
+            element.style.display = 'none'
+            document.body.appendChild(element)
+
+            element.click()
+
+            document.body.removeChild(element)
+          },
+          {
+            startDateString,
+            endDateString,
+            filename,
+            searchText
+          }
+        )
+
+        return `
+            ${fn.inject}
+            <button onclick="${fn.call}">Download CSV</button>
+          `
+      }
+    })
+  }
+
+  // Wait all subviews to be added
+  setTimeout(() => {
+    for (const metricId of Object.keys(bp.experimental.successTelemetryRegistry)) {
+        const metric = bp.experimental.successTelemetryRegistry[metricId]
+        generateButton(metric.key, metric.key, metric.options.name)
+        for (const subViewKey of Object.keys(metric.subviews)) {
+          const subView = metric.subviews[subViewKey]
+          generateButton(subView.label.replace(/ /g, ''), metric.key, subView.label)
+        }
+    }
+  }, 3000)
