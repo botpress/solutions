9b9e8bc52f4797a8100d749477786d5e99434516
Sebastien Buron <sebastien.buron@botpress.com>
2023-05-01 11:55:12 -0400

    Adding converse messaging hook for cummunity

---
+++ custom solutions/Push ConverseAPI messages to messaging/hooks/after_incoming_middleware/insertConverseIntoMessagingIncoming.js
@@ -0,0 +1,68 @@
+  const run = async () => {
+    try {
+      if (event.channel == 'api') {
+        //Get mapped messaging conversation and user id
+        let { userId, conversationId } = await getConverseMapping(event.botId, event.target)
+        const converseUserId = event.target
+
+        const messaging = bp.messaging.forBot(event.botId)
+
+        let user, conversation
+        if (!conversationId) {
+          // No messaging user or conversation generated yet, create
+          user = await messaging.createUser()
+          conversation = await messaging.createConversation(user.id)
+          await saveConverseMapping(event.botId, converseUserId, user.id, conversation.id)
+        } else {
+          user = await messaging.getUser(userId)
+          conversation = await messaging.getConversation(conversationId)
+        }
+
+        if (!user || !conversation) {
+          throw new Error('Failed to get mapped messaging user or conversation')
+        }
+
+        // This and the error string is the only difference
+        // between the incoming and outgoing scripts
+        await messaging.createMessage(conversation.id, user.id, event.payload)
+      }
+    } catch (e) {
+      bp.logger.error('Failed to insert incoming message into messaging: ' + e.message)
+    }
+  }
+
+  const getConverseMapping = async (botId, converseUserId) => {
+    const cached = bp.converseMappingCache.get(`${botId}_${converseUserId}`)
+    if (cached) {
+      return cached
+    }
+
+    let mapping = {}
+
+    try {
+      const rows = await bp.database('converse_user_map').where({ botId, converseUserId })
+      if (rows.length) {
+        mapping = rows[0]
+        bp.converseMappingCache.set(`${botId}_${converseUserId}`, mapping)
+      }
+    } catch (err) {
+      bp.logger.error('An error occurred while fetching the converse user mapping.', err)
+    }
+    return mapping
+  }
+
+  const saveConverseMapping = async (botId, converseUserId, userId, conversationId) => {
+    const mapping = { botId, converseUserId, userId, conversationId }
+
+    try {
+      await bp.database('converse_user_map').insert(mapping)
+      bp.converseMappingCache.set(`${botId}_${converseUserId}`, mapping)
+      return mapping
+    } catch (err) {
+      bp.logger.error('An error occurred while creating a user mapping.', err)
+
+      return undefined
+    }
+  }
+
+  return run()
